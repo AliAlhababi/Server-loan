fafd4810c0e48067263ee3c0a26a74b82cb19f49
AliAlhababi <a_al7babi2@hotmail.com>
2025-07-31 14:50:30 +0300

    Fix AdminController import case sensitivity

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { verifyToken, requireAdmin } = require('../middleware/auth');
-const adminController = require('../controllers/adminController');
+const adminController = require('../controllers/AdminController');
 const BackupController = require('../controllers/BackupController');
 
 const router = express.Router();
